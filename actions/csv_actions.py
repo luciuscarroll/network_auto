91ff8f51ad89a7385a7a3c1a186bbfa88a7e22fe
Lucius Carroll <hagildon@gmail.com>
2022-10-30 15:07:22 -0600

    push for debug

--- actions/csv_actions.py
+++ actions/csv_actions.py
@@ -1,2 +1,8 @@
 import csv
 
+with open ('networkauto/actions/usernames.csv', newline='') as csvfile:
+    usernamereader = csv.reader(csvfile, delimiter=',')
+    usernames = []
+    for row in usernamereader:
+        usernames.append
+        print()
